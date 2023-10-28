0371efdaba78008b21b100d60e9131f90dd6e456
Maroun Baydoun <maroun.baydoun@gmail.com>
2023-10-28 16:25:06 +0200

    update values when player in water

--- player.gd
+++ player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,17 +13,23 @@ var original_gravity = gravity
 var is_in_water = false
 
 var current_direction = 0
+var current_speed = SPEED
+var current_jump_velocity = JUMP_VELOCITY
 
 func _ready():
 	_animated_sprite.play("idle")
 
 func has_entred_water() :
 	is_in_water = true
-	gravity *= 0.9
+	gravity = original_gravity * 0.5
+	current_speed = SPEED * 0.7
+	current_jump_velocity = JUMP_VELOCITY * 0.9
 	
 func has_exited_water() :
 	is_in_water = false
 	gravity = original_gravity 
+	current_speed = SPEED
+	current_jump_velocity = JUMP_VELOCITY
 
 
 func _process(delta):
@@ -46,17 +52,14 @@ func _physics_process(delta):
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = current_jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
 		
-	
-		
-
 	move_and_slide()
