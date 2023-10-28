82140f8f82fe7a8b576b717ece143934d946d3f1
dareios99 <dario.goldgel@gmail.com>
2023-10-28 16:08:39 +0200

    signals fix

--- main.gd
+++ main.gd
@@ -4,6 +4,16 @@ extends Node2D
 
 
 func _ready() -> void:
-	$water.area_entered.connect($CharacterBody2D.has_entred_water)
-	$water.area_exited.connect($CharacterBody2D.has_exited_water)
 	
+	$water.body_entered.connect(body_in_water.bind(true))
+	$water.body_exited.connect(body_in_water.bind(false))
+	
+	
+func body_in_water(body, entered:bool):
+	
+	if (body is CharacterBody2D):
+		if (entered):
+			body.has_entred_water()
+		else:
+			body.has_exited_water()
+
